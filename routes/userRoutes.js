286f4176d746707deb07417bce9ad6fedd6eb948
Abdelrahman kamal <abdelrahmankamal776@gmail.com>
2025-03-14 15:50:34 +0200

    feat : add register, confirm-email, login routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,22 +6,11 @@ const { verifyToken, isLoggedIn, verifyGoogleToken, mockVerifyToken } = require(
 const userController = require('../controllers/userController');
 
 // router.post('/register', userController.registerUser)
-<<<<<<< HEAD
 router.route('/').post(userController.registerUser);
 
 // router.route('/confirm-email').patch(mockVerifyToken , userController.confirmEmail)
 router.route('/confirm-email').patch(userController.confirmEmail);
 router.route('/login').post(userController.login);
-
-
-
-=======
-router.route('/').post(userController.registerUser)
->>>>>>> 2e4c884ebc3902ba26dd3fa1c3e5447b60084487
-
-// router.route('/confirm-email').patch(mockVerifyToken , userController.confirmEmail)
-router.route('/confirm-email').patch(userController.confirmEmail)
-router.route('/login').post(userController.login)
 
 
 
