681e5e4f9b75ee18e009279f8c46587bc1d5ae01
Omar Elshereef <omarelshereef@gmail.com>
2025-03-15 14:09:14 +0200

    feature forget password: add core components with sending email not handled yet

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,25 +1,27 @@
-const userModel = require('../models/userModel');
-const postModel = require('../models/postModel');
-const commentModel = require('../models/commentModel');
-const repostModel = require('../models/repostModel');
-const reportModel = require('../models/reportModel');
+const userModel = require("../models/userModel");
+const postModel = require("../models/postModel");
+const commentModel = require("../models/commentModel");
+const repostModel = require("../models/repostModel");
+const reportModel = require("../models/reportModel");
 
-const axios = require('axios');
-const bcrypt = require('bcryptjs');
-const mongoose = require('mongoose');
-const jwt = require('jsonwebtoken');
-require('dotenv').config();
-const { sendEmailConfirmation } = require('../utils/emailconfirmationService');
-const { validateEmail, validatePassword } = require('../utils/validateEmailPassword');
-const { generateTokens } = require('./jwtController');
+const axios = require("axios");
+const bcrypt = require("bcryptjs");
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const { sendEmailConfirmation } = require("../utils/emailconfirmationService");
+const {
+  validateEmail,
+  validatePassword,
+} = require("../utils/validateEmailPassword");
+const { generateTokens } = require("./jwtController");
 
 const dummyData = async (req, res) => {
-    res.status(200).json({ message: 'Dummy data' });
+  res.status(200).json({ message: "Dummy data" });
 };
 
-
 const getUser = async (req, res) => {
-    res.status(200).json({ message: 'Dummy data' });
+  res.status(200).json({ message: "Dummy data" });
 };
 
 // const createUser = async (req, res) => {
@@ -27,220 +29,279 @@ const getUser = async (req, res) => {
 // };
 
 const sendConnectionRequest = async (req, res) => {
-    res.status(200).json({ message: 'Dummy data' });
+  res.status(200).json({ message: "Dummy data" });
 };
 
 const acceptConnectionRequest = async (req, res) => {
-    res.status(200).json({ message: 'Dummy data' });
+  res.status(200).json({ message: "Dummy data" });
 };
 
 const declineConnectionRequest = async (req, res) => {
-    res.status(200).json({ message: 'Dummy data' });
+  res.status(200).json({ message: "Dummy data" });
 };
 
 const removeConnection = async (req, res) => {
-    res.status(200).json({ message: 'Dummy data' });
+  res.status(200).json({ message: "Dummy data" });
 };
 
 const addProfilePicture = async (req, res) => {
-    res.status(200).json({ message: 'Dummy data' });
+  res.status(200).json({ message: "Dummy data" });
 };
 
 const updateProfilePicture = async (req, res) => {
-    res.status(200).json({ message: 'Dummy data' });
+  res.status(200).json({ message: "Dummy data" });
 };
 
 const removeProfilePicture = async (req, res) => {
-    res.status(200).json({ message: 'Dummy data' });
+  res.status(200).json({ message: "Dummy data" });
 };
 
 const addCoverPicture = async (req, res) => {
-    res.status(200).json({ message: 'Dummy data' });
+  res.status(200).json({ message: "Dummy data" });
 };
 
 const updateCoverPicture = async (req, res) => {
-    res.status(200).json({ message: 'Dummy data' });
+  res.status(200).json({ message: "Dummy data" });
 };
 
 const removeCoverPicture = async (req, res) => {
-    res.status(200).json({ message: 'Dummy data' });
+  res.status(200).json({ message: "Dummy data" });
 };
 
 const addResume = async (req, res) => {
-    res.status(200).json({ message: 'Dummy data' });
+  res.status(200).json({ message: "Dummy data" });
 };
 
 const updateResume = async (req, res) => {
-    res.status(200).json({ message: 'Dummy data' });
+  res.status(200).json({ message: "Dummy data" });
 };
 
 const removeResume = async (req, res) => {
-    res.status(200).json({ message: 'Dummy data' });
+  res.status(200).json({ message: "Dummy data" });
 };
-
 
 const registerUser = async (req, res) => {
-    try {
-
-        // get registration data
-        const { firstName, lastName, email, password, recaptchaResponseToken } = req.body;
-        if (!firstName || !lastName || !email || !password || !recaptchaResponseToken) {
-            return res.status(400).json({ message: 'all fields are required' });
-        }
-        //validate email format and password requirements
-        const isValidEmail = validateEmail(email);
-        if (!isValidEmail) {
-            return res.status(400).json({ message: 'Email not valid, Write a valid email' });
-        }
-        const isValidPassword = validatePassword(password);
-        if (!isValidPassword) {
-            return res.status(400).json({
-                message: 'Ensure the password contains at least 1 digit, 1 lowercase,1 uppercase letter, and is at least 8 characters long.'
-            });
-        }
-
-        //verify CAPTCHA
-        const captchaVerificationUrl = `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SECRET_KEY}&response${recaptchaResponseToken}`;
-        const captchaResponseData = await axios.post(captchaVerificationUrl);
-        if (!captchaResponseData.data.success) {
-            return res.status(400).json({
-                success: false,
-                message: 'CAPCHA Verification failed'
-            });
-        }
-
-        //check if user already exists
-        const existingUser = await userModel.findOne({ email });
-        if (existingUser) {
-            return res.status(409).json({ message: 'The User already exist use another email' });
-        }
-
-        //Hash the Password
-        const hashsalt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(password, hashsalt);
-
-        //start a session
-        const session = await mongoose.startSession();
-        session.startTransaction();
-        //Create new User
-        const newUser = await userModel.create([{ firstName, lastName, email, password: hashedPassword, isEmailConfirmed: false }], { session });
-
-        // use generateTokens from jwt controller and fix it ✅
-        const jwtrefreshToken = generateTokens(newUser[0], res);
-        console.log(`refresh tokeen:  ${jwtrefreshToken}`);
-        await session.commitTransaction();
-        session.endSession();
-
-        // remove get confirm-email end point and directly send from here ✅
-        const isEmailSent = await sendEmailConfirmation(newUser[0]._id);
-        if (!isEmailSent.success) {
-            return res.status(400).json({
-                success: false,
-                message: isEmailSent.error
-            });
-        }
-        return res.status(201).json({
-            success: true,
-            message: 'User registered successfully. Please check your email to confirm your account.',
-            data: {
-                token: jwtrefreshToken,
-                user: newUser[0]
-            }
-        });
+  try {
+    // get registration data
+    const { firstName, lastName, email, password, recaptchaResponseToken } =
+      req.body;
+    if (
+      !firstName ||
+      !lastName ||
+      !email ||
+      !password ||
+      !recaptchaResponseToken
+    ) {
+      return res.status(400).json({ message: "all fields are required" });
     }
-    catch (error) {
-        await session.abortTransaction();
-        session.endSession();
-        // console.error(error.statusCode);
-        return res.status(error.statusCode || 500).json({
-            success: false, message: 'Registration failed',
-            error: `${error.message}`
-        });
-
+    //validate email format and password requirements
+    const isValidEmail = validateEmail(email);
+    if (!isValidEmail) {
+      return res
+        .status(400)
+        .json({ message: "Email not valid, Write a valid email" });
     }
+    const isValidPassword = validatePassword(password);
+    if (!isValidPassword) {
+      return res.status(400).json({
+        message:
+          "Ensure the password contains at least 1 digit, 1 lowercase,1 uppercase letter, and is at least 8 characters long.",
+      });
+    }
+
+    //verify CAPTCHA
+    const captchaVerificationUrl = `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SECRET_KEY}&response${recaptchaResponseToken}`;
+    const captchaResponseData = await axios.post(captchaVerificationUrl);
+    if (!captchaResponseData.data.success) {
+      return res.status(400).json({
+        success: false,
+        message: "CAPCHA Verification failed",
+      });
+    }
+
+    //check if user already exists
+    const existingUser = await userModel.findOne({ email });
+    if (existingUser) {
+      return res
+        .status(409)
+        .json({ message: "The User already exist use another email" });
+    }
+
+    //Hash the Password
+    const hashsalt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, hashsalt);
+
+    //start a session
+    const session = await mongoose.startSession();
+    session.startTransaction();
+    //Create new User
+    const newUser = await userModel.create(
+      [
+        {
+          firstName,
+          lastName,
+          email,
+          password: hashedPassword,
+          isEmailConfirmed: false,
+        },
+      ],
+      { session }
+    );
+
+    // use generateTokens from jwt controller and fix it ✅
+    const jwtrefreshToken = generateTokens(newUser[0], res);
+    console.log(`refresh tokeen:  ${jwtrefreshToken}`);
+    await session.commitTransaction();
+    session.endSession();
+
+    // remove get confirm-email end point and directly send from here ✅
+    const isEmailSent = await sendEmailConfirmation(newUser[0]._id);
+    if (!isEmailSent.success) {
+      return res.status(400).json({
+        success: false,
+        message: isEmailSent.error,
+      });
+    }
+    return res.status(201).json({
+      success: true,
+      message:
+        "User registered successfully. Please check your email to confirm your account.",
+      data: {
+        token: jwtrefreshToken,
+        user: newUser[0],
+      },
+    });
+  } catch (error) {
+    await session.abortTransaction();
+    session.endSession();
+    // console.error(error.statusCode);
+    return res.status(error.statusCode || 500).json({
+      success: false,
+      message: "Registration failed",
+      error: `${error.message}`,
+    });
+  }
 };
 
-
 const confirmEmail = async (req, res) => {
-    try {
-        const { emailVerificationToken } = req.body;
-        if (!emailVerificationToken) {
-            return res.status(400).json({
-                success: false,
-                message: "Verification token is required"
-            });
-        }
-
-        const verificationDate = new Date(Date.now());
-        const user = await userModel.findOne({
-            emailVerificationToken: emailVerificationToken,
-            emailVerificationExpiresAt: { $gt: verificationDate }
-        });
-
-        if (!user) {
-            return res.status(400).json({
-                success: false,
-                message: 'Invalid or expired token'
-            });
-        }
-        user.isConfirmed = true;
-        user.emailVerificationToken = null;
-        user.emailVerificationExpiresAt = null;
-        await user.save();
-
-        return res.status(200).json({
-            success: true,
-            message: "Email is confirmed successfully"
-        });
+  try {
+    const { emailVerificationToken } = req.body;
+    if (!emailVerificationToken) {
+      return res.status(400).json({
+        success: false,
+        message: "Verification token is required",
+      });
     }
 
-    catch (error) {
-        return res.status(500).json({
-            success: false,
-            message: "Email confirmation failed",
-            error: error.message
-        });
+    const verificationDate = new Date(Date.now());
+    const user = await userModel.findOne({
+      emailVerificationToken: emailVerificationToken,
+      emailVerificationExpiresAt: { $gt: verificationDate },
+    });
+
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid or expired token",
+      });
     }
+    user.isConfirmed = true;
+    user.emailVerificationToken = null;
+    user.emailVerificationExpiresAt = null;
+    await user.save();
+
+    return res.status(200).json({
+      success: true,
+      message: "Email is confirmed successfully",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Email confirmation failed",
+      error: error.message,
+    });
+  }
 };
 
 const login = async (req, res, next) => {
-    try {
-        const { email, password } = req.body;
+  try {
+    const { email, password } = req.body;
 
-        const user = await User.findOne({ email });
+    const user = await User.findOne({ email });
 
-        if (!user) {
-            const error = new Error('User not found');
-            error.statusCode = 404;
-            throw error;
-        }
-
-        const isPasswordValid = await bcrypt.compare(password, user.password);
-
-        if (!isPasswordValid) {
-            const error = new Error('Invalid password');
-            error.statusCode = 401;
-            throw error;
-        }
-
-        const token = jwt.sign({ userId: user._id }, JWT_SECRET, { expiresIn: JWT_EXPIRES_IN });
-
-        res.status(200).json({
-            success: true,
-            message: 'User signed in successfully',
-            data: {
-                token,
-                user,
-            }
-        });
-    } catch (error) {
-        ;
+    if (!user) {
+      const error = new Error("User not found");
+      error.statusCode = 404;
+      throw error;
     }
-};
-module.exports = {
-    dummyData,
-    registerUser,
-    confirmEmail,
-    login
 
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      const error = new Error("Invalid password");
+      error.statusCode = 401;
+      throw error;
+    }
+
+    const token = jwt.sign({ userId: user._id }, JWT_SECRET, {
+      expiresIn: JWT_EXPIRES_IN,
+    });
+
+    res.status(200).json({
+      success: true,
+      message: "User signed in successfully",
+      data: {
+        token,
+        user,
+      },
+    });
+  } catch (error) {}
+};
+
+const forgorPassword = async (req, res) => {
+  try {
+    const user = await userModel.findOne({ email: req.body.email });
+    if (!user) {
+      const error = new Error("There is no such email address");
+      error.statusCode = 404;
+      throw error;
+    }
+    const resetToken = user.createPasswordResetToken();
+    await user.save({ validateBeforeSave: false });
+    const resetURL = `${req.protocol}://${req.get(
+      "host"
+    )}/user/reset-password/${resetToken} `;
+    const message = `forgot your password? Submit a patch request with your new password to: ${resetURL} .\n If you didn't forget your password, ignore this email`;
+    try {
+      /* await sendEmail({
+            email: user.email,
+            subject: 'your password reset token (valid for 10 mins)',
+            message,
+        }); */
+
+      res.status(200).json({
+        status: "success",
+        message: "Token sent to email",
+        resetToken,
+      });
+    } catch (err) {
+      console.log(err);
+      user.passwordResetToken = undefined;
+      user.passwordResetExpiresAt = undefined;
+      await user.save({ validateBeforeSave: false });
+      const error = new Error("There was an error sending the email");
+      error.statusCode = 500;
+      throw error;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = {
+  dummyData,
+  registerUser,
+  confirmEmail,
+  login,
+  forgorPassword,
 };
--- models/userModel.js
+++ models/userModel.js
@@ -1,8 +1,9 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const bcrypt = require('bcryptjs');
-
-const userSchema = new mongoose.Schema({
+const bcrypt = require("bcryptjs");
+const crypto = require("crypto");
+const userSchema = new mongoose.Schema(
+  {
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
     email: { type: String, unique: true, required: true },
@@ -15,29 +16,59 @@ const userSchema = new mongoose.Schema({
     lastJobTitle: { type: String, default: null },
     industry: { type: String, default: null },
     certificates: [{ type: String }],
-    workExperience: [{
+    workExperience: [
+      {
         jobTitle: { type: String, required: true },
         companyName: { type: String, required: true },
         fromDate: { type: Date, required: true },
         toDate: { type: Date },
-        employmentType: { type: String, enum: ["Full Time", "Part Time", "Freelance", "Self Employed", "Contract", "Internship", "Apprenticeship", "Seasonal"], required: true },
+        employmentType: {
+          type: String,
+          enum: [
+            "Full Time",
+            "Part Time",
+            "Freelance",
+            "Self Employed",
+            "Contract",
+            "Internship",
+            "Apprenticeship",
+            "Seasonal",
+          ],
+          required: true,
+        },
         location: { type: String },
         locationType: { type: String, enum: ["Onsite", "Hybrid", "Remote"] },
         description: { type: String },
-        foundVia: { type: String, enum: ["Indeed", "LinkedIn", "Company Website", "Other job sites", "Referral", "Contracted by Recruiter", "Staffing Agency", "Other"] },
+        foundVia: {
+          type: String,
+          enum: [
+            "Indeed",
+            "LinkedIn",
+            "Company Website",
+            "Other job sites",
+            "Referral",
+            "Contracted by Recruiter",
+            "Staffing Agency",
+            "Other",
+          ],
+        },
         skills: [{ type: String }],
-        media: { type: String }
-    }],
+        media: { type: String },
+      },
+    ],
     mainEducation: {
-        type: Number,  // Index of the education in the education array
-        default: null,    // First education entry is default
+      type: Number, // Index of the education in the education array
+      default: null, // First education entry is default
     },
-    skills: [{
+    skills: [
+      {
         skillName: { type: String, required: true },
-        endorsements: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }]
-    }],
+        endorsements: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+      },
+    ],
 
-    education: [{
+    education: [
+      {
         school: { type: String, required: true },
         degree: { type: String },
         fieldOfStudy: { type: String },
@@ -47,49 +78,91 @@ const userSchema = new mongoose.Schema({
         activities: { type: String },
         description: { type: String },
         skills: [{ type: String }],
-        media: { type: String }
-    }],
-    transactions: [{ type: mongoose.Schema.Types.ObjectId, ref: "Subscription" }],
+        media: { type: String },
+      },
+    ],
+    transactions: [
+      { type: mongoose.Schema.Types.ObjectId, ref: "Subscription" },
+    ],
     subscription: { type: mongoose.Schema.Types.ObjectId, ref: "Subscription" },
     isPremium: { type: Boolean, default: false },
     companies: [{ type: mongoose.Schema.Types.ObjectId, ref: "Company" }],
-    adminInCompanies: [{ type: mongoose.Schema.Types.ObjectId, ref: "Company" }],
+    adminInCompanies: [
+      { type: mongoose.Schema.Types.ObjectId, ref: "Company" },
+    ],
     isSuperAdmin: { type: Boolean, default: false },
-    profilePrivacySettings: { type: String, enum: ["public", "private", "connectionsOnly"], default: "public" },
-    connectionRequestPrivacySetting: { type: String, enum: ["everyone", "connectionsOnly"], default: "everyone" },
+    profilePrivacySettings: {
+      type: String,
+      enum: ["public", "private", "connectionsOnly"],
+      default: "public",
+    },
+    connectionRequestPrivacySetting: {
+      type: String,
+      enum: ["everyone", "connectionsOnly"],
+      default: "everyone",
+    },
     impressions: [{ type: mongoose.Schema.Types.ObjectId, ref: "Impression" }],
     posts: [{ type: mongoose.Schema.Types.ObjectId, ref: "Post" }],
     comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
     reposts: [{ type: mongoose.Schema.Types.ObjectId, ref: "Repost" }],
-    following: [{
-        entity: { type: mongoose.Schema.Types.ObjectId, refPath: 'following.entityType' },
-        entityType: { type: String, enum: ['User', 'Company'] },
-        followedAt: { type: Date, default: Date.now }
-    }],
-    followers: [{
-        entity: { type: mongoose.Schema.Types.ObjectId, refPath: 'followers.entityType' },
-        entityType: { type: String, enum: ['User', 'Company'] },
-        followedAt: { type: Date, default: Date.now }
-    }],
+    following: [
+      {
+        entity: {
+          type: mongoose.Schema.Types.ObjectId,
+          refPath: "following.entityType",
+        },
+        entityType: { type: String, enum: ["User", "Company"] },
+        followedAt: { type: Date, default: Date.now },
+      },
+    ],
+    followers: [
+      {
+        entity: {
+          type: mongoose.Schema.Types.ObjectId,
+          refPath: "followers.entityType",
+        },
+        entityType: { type: String, enum: ["User", "Company"] },
+        followedAt: { type: Date, default: Date.now },
+      },
+    ],
     connectionList: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
     blockedUsers: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
     profileViews: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
     savedPosts: [{ type: mongoose.Schema.Types.ObjectId, ref: "Post" }],
     savedJobs: [{ type: mongoose.Schema.Types.ObjectId, ref: "Job" }],
     isConfirmed: { type: Boolean, default: false },
-    appliedJobs: [{
-        jobId: { type: mongoose.Schema.Types.ObjectId, ref: "Job", required: true },
-        status: { type: String, enum: ["pending", "viewed", "rejected", "accepted"], default: "pending" }
-    }],
-    sentConnectionRequests: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-    receivedConnectionRequests: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    appliedJobs: [
+      {
+        jobId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Job",
+          required: true,
+        },
+        status: {
+          type: String,
+          enum: ["pending", "viewed", "rejected", "accepted"],
+          default: "pending",
+        },
+      },
+    ],
+    sentConnectionRequests: [
+      { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    ],
+    receivedConnectionRequests: [
+      { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    ],
     messageRequests: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-    chats: [{
-        chatId: { type: mongoose.Schema.Types.ObjectId, refPath: 'chats.chatType' },
-        chatType: { type: String, enum: ['DirectChat', 'ChatGroup'] },
+    chats: [
+      {
+        chatId: {
+          type: mongoose.Schema.Types.ObjectId,
+          refPath: "chats.chatType",
+        },
+        chatType: { type: String, enum: ["DirectChat", "ChatGroup"] },
         unreadCount: { type: Number, default: 0 },
-        lastReadAt: { type: Date }
-    }],
+        lastReadAt: { type: Date },
+      },
+    ],
     defaultMode: { type: String, enum: ["light", "dark"], default: "light" },
     googleId: { type: String, default: null },
     fcmToken: { type: String },
@@ -99,8 +172,21 @@ const userSchema = new mongoose.Schema({
     passwordResetToken: { type: String, default: null },
     passwordResetExpiresAt: { type: Date },
 
-    isActive: { type: Boolean, default: true }
-}, { timestamps: true }); // Adds createdAt & updatedAt automatically
-module.exports = mongoose.model('User', userSchema);
+    isActive: { type: Boolean, default: true },
+  },
+  { timestamps: true }
+); // Adds createdAt & updatedAt automatically
 
+userSchema.methods.createPasswordResetToken = function () {
+  const resetToken = crypto.randomBytes(32).toString("hex");
 
+  this.passwordResetToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
+
+  this.passwordResetExpiresAt = Date.now() + 10 * 60 * 1000;
+  return resetToken;
+};
+
+module.exports = mongoose.model("User", userSchema);
