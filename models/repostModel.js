a908cbe0fc7bbc634ca0151048eb30c53747e21e
Alii-s <ali.moh03@outlook.com>
2025-03-24 23:07:10 +0200

    feat: add tagged users to reposts

--- models/repostModel.js
+++ models/repostModel.js
@@ -3,6 +3,13 @@ const repostSchema = new mongoose.Schema({
     userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
     postId: { type: mongoose.Schema.Types.ObjectId, ref: "Post", required: true },
     description: { type: String },
+    taggedUsers: [{
+      userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+      userType: { type: String, enum: ["User", "Company"], default: "User" },
+      firstName: { type: String },
+      lastName: { type: String },
+      companyName: { type: String },
+    }],
     isActive: { type: Boolean, default: true }
   }, { timestamps: true }); // Adds createdAt & updatedAt automatically
   
