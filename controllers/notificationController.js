ad9d30d1308cfedf26ec173ceeb697a44239e44e
Omar Elshereef <omarelshereef@gmail.com>
2025-03-27 23:59:44 +0200

    feat: implement getNotifications function and update notification subject types

--- controllers/notificationController.js
+++ controllers/notificationController.js
@@ -3,7 +3,22 @@
 const Notification = require("../models/notificationModel");
 const userModel = require("../models/userModel");
 
-const getNotifications = async (req, res) => {};
+const getNotifications = async (req, res) => {
+  try {
+    const userId = req.user.id; // Assuming you have a middleware that sets req.user
+    const notifications = await Notification.find({ to: userId }).sort({
+      createdAt: -1,
+    });
+
+    if (!notifications) {
+      return res.status(404).json({ message: "No notifications found" });
+    }
+
+    return res.status(200).json(notifications);
+  } catch (error) {
+    return res.status(500).json({ message: "Internal server Error" });
+  }
+};
 
 module.exports = {
   getNotifications,
