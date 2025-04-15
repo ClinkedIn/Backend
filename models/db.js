6d272d20360af68c6f484295d80e7a33d48e7640
tahaaa22 <ahmed.taha.oman@gmail.com>
2025-04-15 03:29:15 +0200

    fix: update MongoDB connection URI to remove fallback to localhost

--- models/db.js
+++ models/db.js
@@ -1,10 +1,11 @@
+require("dotenv").config();
 const process = require("process");
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
     // const uri = "mongodb://localhost:27017/LockedIn";
-    const uri = process.env.MONGODB_CONNECTION || "mongodb://0.0.0.0:27017/LockedIn";
+    const uri = process.env.MONGODB_CONNECTION ;
     await mongoose.connect(uri);
     console.log("✅ MongoDB Connected Successfully");
   } catch (error) {
