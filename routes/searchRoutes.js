15b0c1ae340d8764707f87f4687ca0fcb4fd4a29
Alii-s <ali.moh03@outlook.com>
2025-04-26 18:21:48 +0300

    feature: search post by keyword + fix swagger documentaiton

--- routes/searchRoutes.js
+++ routes/searchRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const userController = require("../controllers/userController");
 const searchController = require("../controllers/searchController");
+const postController = require("../controllers/postController");
 const comment = require("../controllers/commentController");
 const { protect, mockVerifyToken } = require('../middlewares/auth');
 const upload = require('../middlewares/multer');
@@ -11,3 +12,5 @@ router.route('/users')
 router.route('/jobs')
     .get(searchController.searchJobs);
 module.exports = router;
+router.route('/posts')
+    .get(protect, postController.searchPostsByKeyword);
